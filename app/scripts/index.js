e0dcb0c86a214e939c6863d511eb0cf20bb8e81c
Erik <sanchezerik78@yahoo.com>
2016-04-12 13:49:06 -0400

    working

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,24 +2,35 @@ var _ = require('underscore');
 var handlebars = require('handlebars');
 var $ = require('jquery');
 
-var url = 'https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=whiskey&includes=Images,Shop&sort_on=score';
-var results = $.ajax(url).then(start);
+var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=whiskey&includes=Images,Shop";
+function fetchJSONP(url, callback) {
+  var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+  var script = document.createElement('script');
+
+  window[callbackName] = function(data) {
+    delete window[callbackName];
+    document.body.removeChild(script);
+    callback(data);
+  };
+
+  script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+  document.body.appendChild(script);
+
+}
+
 
 function start(data){
   console.log(data);
-
   displayTiles(data);
 }
 
-function displayTiles(data){
-  var tiles = data.data.results;
-  tiles.forEach(displayTiles);
-}
+fetchJSONP(url, start);
+
 
 function displayTiles(tile){
   var source = $('#tile-template').html();
   var template = handlebars.compile(source);
-  var $tileHtml = $(template(tiles));
+  var html = template(tile);
 
-$('.tiles').append($tileHtml);
+$('.tiles').append(html);
 }
